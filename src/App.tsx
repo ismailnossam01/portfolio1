6862223afd22f04eba2fab5e8b9b42a9103f09e4
ismail_nossam_01 <138509028+ismailnossam01@users.noreply.github.com>
2025-05-02 11:46:44 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,7 @@ function App() {
     </a>
   </div>
   <a
-  href="https://drive.google.com/file/d/18ltKuldV15qIIcc7fK2ONmR-SbP2OA6M/view?usp=drive_link"
+  href="https://drive.google.com/file/d/1CvYStMh1hTN8fkeqrjZtcTlE-K7ZpTBR/view?usp=sharing"
   target="_blank"
   rel="noopener noreferrer"
   className="inline-flex items-center justify-center px-6 py-3 rounded-full border-2 border-blue-400 text-blue-400 font-semibold hover:bg-blue-400 hover:text-white transition-all duration-200 transform hover:-translate-y-1 w-full"
